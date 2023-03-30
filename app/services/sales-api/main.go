2be4802a44b8f17c83f384bf009f9626b8e18871
Umed Jadhav <umedj.formal@gmail.com>
2023-03-31 01:07:07 +0530

    [impl] Added basic logger and maxprocs

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,42 +2,53 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
 	"runtime"
-	"syscall"
 
 	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var build = "develop"
+var serviceName = "sales-api"
 
-// setThreadsForService sets the correct number of threads for service based on what is available
-// either by the machine or quotas
-func setThreadsForService() error {
-	if _, err := maxprocs.Set(); err != nil {
-		return fmt.Errorf("maxprocs: %w", err)
+func newLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
 	}
-	return nil
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
 
 func main() {
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Println("maxprocs: %w", err)
-		os.Exit(1)
-	}
-	err := setThreadsForService()
+	log, err := newLogger(serviceName)
 	if err != nil {
-		fmt.Println("unable to set threads for service: %w", err)
+		log.Fatalf("failed to start logger: %w", err)
 		os.Exit(1)
 	}
-	g := runtime.GOMAXPROCS(0)
-	log.Printf("starting service build[%s] CPU[%d]", build, g)
-	defer log.Println("service ended")
+	defer log.Sync()
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
-	log.Println("stopping service")
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	if _, err := maxprocs.Set(); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module cnGoService
 
 go 1.18
 
-require go.uber.org/automaxprocs v1.5.2
+require go.uber.org/zap v1.24.0
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/automaxprocs v1.5.2
+	go.uber.org/multierr v1.6.0 // indirect
+)

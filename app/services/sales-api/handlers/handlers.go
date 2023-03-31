580645544ce60caa9913bc0e3e893205242d83bc
Umed Jadhav <umedj.formal@gmail.com>
2023-03-31 11:58:48 +0530

    [chore] Added basic test api handler

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"expvar"
 	"net/http"
 	"net/http/pprof"
+	"os"
+
+	"github.com/dimfeld/httptreemux/v5"
+	"go.uber.org/zap"
 )
 
 // DebugStandardLibraryMux registers all the debug routes from std library into a new mux
@@ -19,5 +24,26 @@ func DebugStandardLibraryMux() *http.ServeMux {
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	mux.Handle("/debug/vars", expvar.Handler())
 
+	return mux
+}
+
+type APIMuxConfig struct {
+	Shutdown chan os.Signal
+	Log      *zap.SugaredLogger
+}
+
+// APIMux constructs an http.Handler with all application routes defined
+func APIMux(conf APIMuxConfig) *httptreemux.ContextMux {
+	mux := httptreemux.NewContextMux()
+	h := func(w http.ResponseWriter, r *http.Request) {
+		status := struct {
+			Status string
+		}{
+			Status: "OK",
+		}
+		json.NewEncoder(w).Encode(status)
+	}
+
+	mux.Handle(http.MethodGet, "/test", h)
 	return mux
 }
